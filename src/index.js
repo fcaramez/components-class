ce230942ba694a7a99ce95822848018059ced7e6
fcaramez <francisco.caramez.duarte@gmail.com>
2022-08-25 09:12:08 +0100

    Finishes class

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
 import "react-toastify/dist/ReactToastify.css";
+import GlobalStyle from "./styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  color: {
+    primary: "#6419E6",
+    neutral: "#2a303c",
+    base: "#191d24",
+  },
+};
 
 const config = {
   initialColorMode: "dark",
@@ -16,11 +26,12 @@ const chakraTheme = extendTheme({ config });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <ChakraProvider theme={chakraTheme}>
+  <ChakraProvider theme={chakraTheme}>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <App />
-    </ChakraProvider>
-  </React.StrictMode>
+    </ThemeProvider>
+  </ChakraProvider>
 );
 
 reportWebVitals();
